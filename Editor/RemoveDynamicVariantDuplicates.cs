7b191ef247c2d3fd60f5db973c88798e6cef895d
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2026-04-19 17:40:27 -0700

    Added fix for fog variant stripping

--- Editor/RemoveDynamicVariantDuplicates.cs
+++ Editor/RemoveDynamicVariantDuplicates.cs
@@ -8,6 +8,9 @@ using UnityEngine.Rendering;
 using System.Text;
 using UnityEditor;
 using System.Reflection;
+using UnityEditor.Build.Content;
+using Unity.Collections.LowLevel.Unsafe;
+using System.Runtime.InteropServices;
 
 namespace emdl
 {
@@ -21,11 +24,37 @@ namespace emdl
     /// </summary>
 
     // Thanks c# for not having simple numeric logic in preprocessor branches, I love checking 40 bools instead of comparing two numbers
-#if  UNITY_2022_3_0 || UNITY_2022_3_1 || UNITY_2022_3_2 || UNITY_2022_3_3 || UNITY_2022_3_4 || UNITY_2022_3_5 || UNITY_2022_3_6 || UNITY_2022_3_7 || UNITY_2022_3_8 || UNITY_2022_3_9 || UNITY_2022_3_10 || UNITY_2022_3_11 || UNITY_2022_3_12 || UNITY_2022_3_13 || UNITY_2022_3_14 || UNITY_2022_3_15 || UNITY_2022_3_16 || UNITY_2022_3_17 || UNITY_2022_3_18 || UNITY_2022_3_19 || UNITY_2022_3_20 || UNITY_2022_3_21 || UNITY_2022_3_22 || UNITY_2022_3_23 || UNITY_2022_3_24 || UNITY_2022_3_25 || UNITY_2022_3_26 || UNITY_2022_3_27 || UNITY_2022_3_28 || UNITY_2022_3_29 || UNITY_2022_3_30 || UNITY_2022_3_31 || UNITY_2022_3_32 || UNITY_2022_3_33 || UNITY_2022_3_34 || UNITY_2022_3_35 || UNITY_2022_3_36 || UNITY_2022_3_37 || UNITY_2022_3_38 || UNITY_2022_3_39
+#if UNITY_2022_3_0 || UNITY_2022_3_1 || UNITY_2022_3_2 || UNITY_2022_3_3 || UNITY_2022_3_4 || UNITY_2022_3_5 || UNITY_2022_3_6 || UNITY_2022_3_7 || UNITY_2022_3_8 || UNITY_2022_3_9 || UNITY_2022_3_10 || UNITY_2022_3_11 || UNITY_2022_3_12 || UNITY_2022_3_13 || UNITY_2022_3_14 || UNITY_2022_3_15 || UNITY_2022_3_16 || UNITY_2022_3_17 || UNITY_2022_3_18 || UNITY_2022_3_19 || UNITY_2022_3_20 || UNITY_2022_3_21 || UNITY_2022_3_22 || UNITY_2022_3_23 || UNITY_2022_3_24 || UNITY_2022_3_25 || UNITY_2022_3_26 || UNITY_2022_3_27 || UNITY_2022_3_28 || UNITY_2022_3_29 || UNITY_2022_3_30 || UNITY_2022_3_31 || UNITY_2022_3_32 || UNITY_2022_3_33 || UNITY_2022_3_34 || UNITY_2022_3_35 || UNITY_2022_3_36 || UNITY_2022_3_37 || UNITY_2022_3_38 || UNITY_2022_3_39
     class RemoveDynamicVariantDuplicates : IPreprocessShaders
     {
-        public int callbackOrder { get { return 0; } }
-        
+        public int callbackOrder { get { return -1; } }
+        struct BuildUsageTagUnsafe
+        {
+            public uint m_LightmapModesUsed;
+            public uint m_LegacyLightmapModesUsed;
+            public uint m_DynamicLightmapsUsed;
+            public uint m_FogModesUsed;
+            public uint m_BrgShaderStripModeMask;
+            public bool m_ForceInstancingStrip;
+            public bool m_ForceInstancingKeep;
+            public bool m_ShadowMasksUsed;
+            public bool m_SubtractiveUsed;
+            public bool m_HybridRendererPackageUsed;
+            public bool m_BuildForLivelink;
+            public bool m_BuildForServer;
+        }
+
+        static void SupportedFogModes(out bool linear, out bool exp, out bool exp2)
+        {
+            BuildUsageTagGlobal bt = ContentBuildInterface.GetGlobalUsageFromGraphicsSettings();
+            BuildUsageTagUnsafe bu = UnsafeUtility.As<BuildUsageTagGlobal, BuildUsageTagUnsafe>(ref bt);
+
+            // first bit unused?
+            linear = (bu.m_FogModesUsed & 2) != 0;
+            exp    = (bu.m_FogModesUsed & 4) != 0;
+            exp2   = (bu.m_FogModesUsed & 8) != 0;
+        }
+
         public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> keywordPermutations)
         {
             // dynamic_branches do NOT work in the meta pass! use normal keywords!
@@ -37,13 +66,41 @@ namespace emdl
             // Find all dynamic keywords
 
             LocalKeyword[] localKW = ShaderUtil.GetPassKeywords(shader, snippet.pass, snippet.shaderType);
+            
             List<LocalKeyword> dynamicKWs = new List<LocalKeyword>(localKW.Length);
+            if (shader.name != "Unlit/TestFog")
+            {
+                return;
+            }
 
+            bool keepLinear = false, keepExp = false, keepExp2 = false;
+            SupportedFogModes(out keepLinear, out keepExp, out keepExp2);
+
+            //Debug.Log($"Fog Modes: Linear: {keepLinear} Exp: {keepExp} Exp2: {keepExp2}\n");
             for (int i = 0; i < localKW.Length; i++)
             {
                 if (localKW[i].isDynamic)
                 {
-                    dynamicKWs.Add(localKW[i]);
+                    if (localKW[i].name.StartsWith("FOG_"))
+                    {
+                        bool keepKw = false;
+                        switch (localKW[i].name) 
+                        {
+                            case "FOG_LINEAR":  keepKw = keepLinear;    break;
+                            case "FOG_EXP":     keepKw = keepExp;       break;
+                            case "FOG_EXP2":    keepKw = keepExp2;      break;
+                            default:            keepKw = true;          break;
+                        }
+
+                        if (keepKw)
+                        {
+                            dynamicKWs.Add(localKW[i]);
+                        }
+                    }
+                    else
+                    {
+                        dynamicKWs.Add(localKW[i]);
+                    }
                 }
             }
 
@@ -58,10 +115,23 @@ namespace emdl
             int numPermutations = keywordPermutations.Count;
             int currentSize = 0;
 
+            StringBuilder prebuild = new StringBuilder();
+            StringBuilder postbuild = new StringBuilder();
+            StringBuilder kwSet = new StringBuilder();
+
             // move the keywords which have every dynamic keyword turned on to the front of the array
             for (int pIdx = 0; pIdx < numPermutations; ++pIdx)
             {
                 bool isDynVariant = true;
+
+#if DYN_VARIANT_FIX_DEBUG
+                kwSet.Clear();
+                foreach (var kw in keywordPermutations[pIdx].shaderKeywordSet.GetShaderKeywords())
+                {
+                    kwSet.Append(kw.name + " ");
+                }
+                prebuild.Append(kwSet.ToString());
+#endif
 
                 for (int dIdx = 0; dIdx < numDynamic; ++dIdx)
                 {
@@ -72,8 +142,20 @@ namespace emdl
                 {
                     keywordPermutations[currentSize] = keywordPermutations[pIdx];
                     currentSize++;
+#if DYN_VARIANT_FIX_DEBUG
+                    postbuild.Append(kwSet.ToString());
+#endif
                 }
             }
+
+#if DYN_VARIANT_FIX_DEBUG
+            Debug.Log("Pre strip Keywords: \n" + prebuild.ToString());
+            Debug.Log("Post strip Keywords: \n" + postbuild.ToString());
+            if (currentSize == 0)
+            {
+                Debug.LogError("No variants left!");
+            }
+#endif
 
             // Trim off the end of the array, removing all variants without every dynamic keyword set
             keywordPermutations.TryRemoveElementsInRange(currentSize, numPermutations - currentSize, out System.Exception ex);
@@ -81,4 +163,4 @@ namespace emdl
         } 
     }
 #endif
-}
+        }
--- readme.md
+++ readme.md
@@ -2,4 +2,15 @@
 
 Fix for #pragma dynamic_branch in Unity versions before 2022.3.40f1. In these versions, Unity would treat dynamic_branches as normal keywords and create keyword permutations with and without the dynamic_branch keyword. This means that unity would then compile exact copies of the shader with the dynamic keyword "set" and not "set" despite it not being a preprocessor symbol, bloating compile times and completely defeating the point of dynamic branching. This repository adds a shader variant preprocessor that removes the unnecessary duplicate variants. 
 
-Due to a quirk of unity's variant system, unity will refuse to set the value of dynamic_branch variable if it can't find a shader program permutation with that variable as a keyword. Thus this removes all keyword permutations that do not contain every dynamic_branch variable rather than removing the ones that contain them.
+# IMPORTANT
+
+Due to a quirk of unity's variant system, unity will refuse to set the value of dynamic_branch variable if the shader variant does not contain the corresponding keyword. Thus, **this patch removes all variants that do not contain every dynamic_branch keyword**, not the permutations that contain the keywords!
+
+Do not put multiple dynamic_branch keywords on a single line (ie `#pragma _ FOG_LINEAR FOG_EXP FOG_EXP2`) as that will tell unity to compile variants with only one of each keyword. The patch will then remove all variants from the shader because none of them have all the keywords active at once. Instead, put each dynamic branch in a separate pragma:
+```
+#pragma dynamic_branch FOG_LINEAR
+#pragma dynamic_branch FOG_EXP
+#pragma dynamic_branch FOG_EXP2
+```
+
+Additionally any other code that removes variants, for example unity's built in fog, lightmap, and instancing stripping or other IPreprocessShaders scripts, can cause this patch to delete all variants if they run before and remove variants containing all of the dynamic_branch keywords. Fog keywords are handled explicitly by this script and should work, but others will cause issues if turned into dynamic branches. 
